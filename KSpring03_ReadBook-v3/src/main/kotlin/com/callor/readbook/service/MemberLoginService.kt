5b701c12832a12cf5cd30387a8a8e2b45c3bb212
ioo1332 <ioo168400@gmail.com>
2021-11-01 23:42:41 +0900

    springsecurity

---
+++ KSpring03_ReadBook-v3/src/main/kotlin/com/callor/readbook/service/MemberLoginService.kt
@@ -0,0 +1,40 @@
+package com.callor.readbook.service
+
+import com.callor.readbook.models.MemberVO
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+/**
+ *
+ * Security login Service 클래스
+ * ( *UserDetailsService )
+ *
+ * Security login Service UserDetailsService 를 상속받는다
+ *
+ */
+@Service
+class MemberLoginService : UserDetailsService {
+
+    // 가상의 member list 생성해 두기
+    private val userList = listOf(
+        MemberVO(username = "callor", password = "1234"),
+        MemberVO(username = "ioo168400", password = "1234"),
+        MemberVO(username = "1234", password = "1234"),
+    )
+
+
+    // findByUserName(username) : UserDetails
+    override fun loadUserByUsername(username: String): UserDetails {
+
+        // 배열.find {] : 배열의 요소중 원하는 값이 담겨 있는가?
+        // 담겨 있으면 해당 값을 return 하고, 없으면 null return
+        val member: MemberVO? = userList.find { it.username == username }
+        member ?: throw UsernameNotFoundException("사용자 ID 가 잘못되었습니다")
+        return member
+
+    }
+
+
+}
